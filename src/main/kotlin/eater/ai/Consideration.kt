4c822b3ea34653390985421a4e1e0c6a7d15a386
Tommie Nygren <tommie.nygren@gmail.com>
2022-07-07 10:48:29 +0200

    Added inverted consideration and a consideration that just checks for a component

--- src/main/kotlin/eater/ai/Consideration.kt
+++ src/main/kotlin/eater/ai/Consideration.kt
@@ -1,6 +1,18 @@
 package eater.ai
 
+import com.badlogic.ashley.core.Component
+import com.badlogic.ashley.core.ComponentMapper
 import com.badlogic.ashley.core.Entity
+import kotlin.reflect.KClass
+
+class DoIHaveThisComponentConsideration<ToCheck: Component>(name:String, toCheck:KClass<ToCheck>): Consideration(name) {
+    val mapper = ComponentMapper.getFor(toCheck.java)
+    override fun normalizedScore(entity: Entity): Float {
+        return if(mapper.has(entity)) 1.0f else 0f
+    }
+}
+
+class InvertedConsideration(name: String, consideration: Consideration):Consideration(name, {entity-> 1f/consideration.scoreFunction(entity) })
 
 open class Consideration(val name: String, val scoreFunction: (entity: Entity) -> Float = { 0f }) {
     open fun normalizedScore(entity: Entity): Float {
