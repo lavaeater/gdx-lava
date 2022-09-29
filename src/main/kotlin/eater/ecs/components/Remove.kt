6d0ead5ea7dbd857971298d6c0d093a0bec82cd1
Tommie Nygren <tommie.nygren@gmail.com>
2022-09-29 08:51:08 +0200

    Added some things from the newest game jam game that fit in the gdx-lava library

---
+++ src/main/kotlin/eater/ecs/components/Remove.kt
@@ -0,0 +1,10 @@
+package eater.ecs.components
+
+import com.badlogic.ashley.core.Component
+import com.badlogic.gdx.utils.Pool.Poolable
+
+class Remove : Component, Poolable {
+    override fun reset() {
+
+    }
+}
