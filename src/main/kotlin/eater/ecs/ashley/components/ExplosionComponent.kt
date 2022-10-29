fa6495777374bb4ca418a56557ebf402962e0aa4
Tommie Nygren <tommie.nygren@gmail.com>
2022-10-29 14:51:59 +0200

    Just a refactoring that happened

--- src/main/kotlin/eater/ecs/ashley/components/ExplosionComponent.kt
+++ src/main/kotlin/eater/ecs/ashley/components/ExplosionComponent.kt
@@ -2,15 +2,9 @@ package eater.ecs.ashley.components
 
 import com.badlogic.ashley.core.Component
 import com.badlogic.ashley.core.Entity
-import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.utils.Pool.Poolable
 import ktx.ashley.mapperFor
 import ktx.math.vec2
-
-data class ExplosionBlorb(val position: Vector2, val startRadius: Float = 1f, val endRadius: Float = 15f, val backColor: Color = Color(1f, 0f, 0f, 0.5f), val frontColor: Color = Color(0.7f, 0.4f, 0f, 0.5f)) {
-    var currentRadius = startRadius
-}
 
 class ExplosionComponent: Component, Poolable {
     var explosionTime = 0.1f
