c625c29da81aa47caf39b36eec706d8afb9fbfc7
Tommie Nygren <tommie.nygren@gmail.com>
2022-11-24 16:44:02 +0100

    More work on the musicians and stuff

--- src/main/kotlin/eater/music/Metronome.kt
+++ src/main/kotlin/eater/music/Metronome.kt
@@ -30,7 +30,9 @@ class Metronome(private val tempo: Float) {
     private val timepiece by lazy { GdxAI.getTimepiece() }
     private val currentTime get() = timepiece.time
     private var startTime = 0f
-    private var playing = false
+    var playing = false
+        private set
+    val notPlaying get() = !playing
 
     val timeBars: Float get() {
         return if(playing) {
@@ -56,4 +58,34 @@ class Metronome(private val tempo: Float) {
     }
 }
 
-class Conductor(private val metronome: Metronome, )
+abstract class Musician(protected val metronome: Metronome, var intensity: Float) {
+    lateinit var currentChord: Chord
+        private set
+    var lastTimeBars = 0f
+    fun setChord(chord: Chord) {
+        currentChord = chord
+    }
+
+    abstract fun updateNotes(timeBars: Float)
+}
+
+class Drummer(metronome: Metronome, intensity: Float):Musician(metronome,intensity) {
+    override fun updateNotes(timeBars: Float) {
+        TODO("Not yet implemented")
+    }
+}
+
+class Conductor(private val metronome: Metronome, private val chords: Array<Chord>, private val chordLengthBars: Float, private val musicians: Array<Musician>) {
+    fun update() {
+        if(metronome.notPlaying)
+            return
+
+        val timeBars = metronome.timeBars
+        val chordTimeBars = timeBars % chordLengthBars
+        val currentChord = chords.first { it.barPos > chordTimeBars }
+        for (musician in musicians) {
+            musician.setChord(currentChord)
+            musician.updateNotes(timeBars)
+        }
+    }
+}
