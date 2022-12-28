02049c2ff3419387a76d5fc00b36ec99d5b2f654
Tommie Nygren <tommie.nygren@gmail.com>
2022-12-28 07:19:42 +0100

    Removed serializer, and music stuff

--- src/main/kotlin/eater/music/PlayableNote.kt
+++
@@ -1,5 +0,0 @@
-package eater.music
-
-import de.pottgames.tuningfork.SoundSource
-
-data class PlayableNote(val soundSource: SoundSource, val pitch: Float, val targetTime: Float)
--- src/main/kotlin/eater/music/Sampler.kt
+++
@@ -1,9 +0,0 @@
-package eater.music
-
-import de.pottgames.tuningfork.SoundSource
-
-class Sampler(private val soundSource: SoundSource) {
-    fun play(midiNoteDiff: Int, scheduledTime: Float) {
-        ToPlay.soundsToPlay.add(PlayableNote(soundSource, midiNoteDiff.toPitch(), scheduledTime))
-    }
-}
