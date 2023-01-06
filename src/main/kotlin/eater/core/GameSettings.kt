9497ce4263ea216842e421648a1767d49022e006
Tommie Nygren <tommie.nygren@gmail.com>
2023-01-07 00:32:21 +0100

    3d experiment

--- src/main/kotlin/eater/core/GameSettings.kt
+++ src/main/kotlin/eater/core/GameSettings.kt
@@ -1,6 +1,6 @@
 package eater.core
 
-class GameSettings(
+open class GameSettings(
     val gameWidth: Float = 36f, aspectRatio: Float = 16f / 9f, val pixelsPerMeter: Float = 4f,
     val timeStep: Float = 1 / 60f, val velocityIterations: Int = 16, val positionIterations: Int = 6
 ) {
