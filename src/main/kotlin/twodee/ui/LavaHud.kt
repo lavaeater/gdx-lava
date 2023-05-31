b35d5f5f8a323bd7ab08709ea27487e30a671b16
Tommie Nygren <tommie.nygren@gmail.com>
2023-05-31 13:00:03 +0200

    Some changes

--- src/main/kotlin/twodee/ui/LavaHud.kt
+++ src/main/kotlin/twodee/ui/LavaHud.kt
@@ -7,7 +7,7 @@ import com.badlogic.gdx.utils.viewport.ExtendViewport
 
 abstract class LavaHud(
     val batch: PolygonSpriteBatch,
-    hudWidth: Float = 360f,
+    hudWidth: Float = 600f,
     aspectRatio: Float = 16f / 9f) {
     private val hudHeight = hudWidth * aspectRatio
     val camera = OrthographicCamera()
