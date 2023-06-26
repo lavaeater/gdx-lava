528c21f1c6195fed331b86f0715ffe054b8fafd4
Tommie Nygren <tommie.nygren@gmail.com>
2023-06-26 23:04:39 +0200

    It seemed that something entirely basic totally escaped me for the longest time

--- src/main/kotlin/twodee/screens/ScreenWithStage.kt
+++ src/main/kotlin/twodee/screens/ScreenWithStage.kt
@@ -1,10 +1,13 @@
 package twodee.screens
 
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.utils.viewport.Viewport
 import twodee.core.MainGame
 
-abstract class ScreenWithStage(mainGame: MainGame): BasicScreen(mainGame) {
+abstract class ScreenWithStage(mainGame: MainGame, viewport: Viewport, camera: OrthographicCamera, batch: PolygonSpriteBatch): BasicScreen(mainGame, camera, viewport, batch) {
     abstract val stage: Stage
     override fun render(delta: Float) {
         super.render(delta)
